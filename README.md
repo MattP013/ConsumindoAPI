913b163f5caa59348b3a649e8002437d4c90bbad
MattP013 <mmateus20pereira@gmail.com>
2021-04-04 14:32:03 -0300

    consumindo api

---
+++ README.md
@@ -0,0 +1,2 @@
+# Consumindo API de previsão de tempo
+Reprodução do projeto da Digital Innovation One 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: 'c6859950c39f08e5b414aee4ad39cef3',
+};
